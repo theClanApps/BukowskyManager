233177488323610609ed0fc692fdf18cc6f4ae9c
Nick Servidio <nservidio@mobiquityinc.com>
2014-11-08 19:32:25 -0500

    Initial Commit

---
+++ BukowskiManagerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BukowskiManagerApp
+//
+//  Created by Nicholas Servidio on 11/8/14.
+//  Copyright (c) 2014 The Clan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BukowskiManagerApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BukowskiManagerApp
+//
+//  Created by Nicholas Servidio on 11/8/14.
+//  Copyright (c) 2014 The Clan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
